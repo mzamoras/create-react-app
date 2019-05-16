a039a76c8a6fe9ed70bcf417ed6ac716c5ad80d7
Mike Zamora <mzamoras@backlogics.com>
2019-05-16 10:47:24 -0500

    Updates

--- packages/react-error-overlay/src/__tests__/mapper.js
+++ packages/react-error-overlay/src/__tests__/mapper.js
@@ -75,7 +75,7 @@ test('bad comes back same', async () => {
       _scriptCode: null,
       columnNumber: 2,
       fileName: 'A',
-      functionName: 'App.componentDidMount',
+      functionName: 'BaseApp.js.componentDidMount',
       lineNumber: 1,
     },
   ]);
